bc516fc4976fd87f740bd1461a3085106955a7f6
Syaiful Rizal Sidiq <127954312+rizal504@users.noreply.github.com>
2024-08-21 23:22:57 +0700

    merge rizal n dimas

---
+++ src/Components/Tentang.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Kantor from "../assets/Pictures/kantor.svg";
+
+function Tentang() {
+  return (
+    <div className="py-10">
+      <div className="text-5xl font-bold px-20 ">
+        <div>Tentang Disdukcapil</div>
+        <div className="text-[#D24545]">Kota Semarang</div>
+      </div>
+      <div className="flex justify-center items-center gap-10 px-20">
+      <div className="text-justify pt-10 text-2xl">
+        Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) Kota Semarang
+        merupakan salah satu Organisasi Perangkat Daerah Kota Semarang yang
+        bertanggung jawab melaksanakan urusan pemerintahan daerah terkait
+        pelayanan administrasi kependudukan dan pencatatan sipil. Tugas pokok
+        ini dilaksanakan berdasarkan prinsip otonomi daerah dan tugas
+        pembantuan, memastikan pelayanan kepada masyarakat dalam hal
+        administrasi kependudukan berjalan efektif dan sesuai ketentuan.
+      </div>
+      <img src={Kantor} alt="" className="w-full" />
+      </div>
+      
+    </div>
+  );
+}
+
+export default Tentang;
